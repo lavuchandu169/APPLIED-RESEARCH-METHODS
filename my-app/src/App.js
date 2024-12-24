1a97afc17183c6dfc381e0f4bbe49e6ed9e59bac
Chandu Lavu <82090789+lavuchandu169@users.noreply.github.com>
2024-12-24 21:14:04 +0000

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Manual from "./components/Manual";
 import VA from "./components/VA";
-import Results from "./components/Results";
+
 
 const App = () => {
   return (
@@ -14,7 +14,6 @@ const App = () => {
         <Route path="/" element={<Manual />} />
         <Route path="/manual" element={<Manual />} />
         <Route path="/va" element={<VA />} />
-        <Route path="/result" element={<Results />} />
       </Routes>
     </Router>
   );
